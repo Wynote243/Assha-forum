1b7c1ab7c1dde4761abcd3e8ead8c3bd4ed46199
Samuel Kabwe <wynotebeta@gmail.com>
2025-12-18 13:26:13 +0100

    Mise à jour db.js et ajout .gitignore

--- config/db.js
+++ config/db.js
@@ -1,11 +1,29 @@
-const mysql = require("mysql2");
+//const mysql = require("mysql2");
+
+//const db = mysql.createConnection({
+    //host: "127.0.0.1",
+   // user: "root",
+   // password: "",
+   // database: "monapp",
+   // port: 3306
+//});
+
+//db.connect(err => {
+    //if (err) {
+       // console.error("❌ Erreur MySQL :", err);
+   // } else {
+        //console.log("✅ MySQL connecté (monapp)");
+  //  }
+//});
+
+//module.exports = db;
 
 const db = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "",
-    database: "monapp",
-    port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306
 });
 
 db.connect(err => {
@@ -17,5 +35,3 @@ db.connect(err => {
 });
 
 module.exports = db;
-
-
